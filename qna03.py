80b7ffbbbd6b1aa5859e3a52256531d1191d3f8e
Stewe17 <228335st@gmail.com>
2021-02-13 14:45:10 +0000

    всё

---
+++ qna03.py
@@ -0,0 +1,9 @@
+a,b,c = input().split()
+a, b, c=int(a), int(b), int(c)
+
+if a + b + c != 180:
+  print("None exist")
+elif (a == b or b == c or c == a) and (a == 90 or b == 90 or c == 90):
+  print(1)
+else:
+  print(0)
