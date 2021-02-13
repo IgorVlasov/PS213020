93eeb812d77c830a4184d1d3eddd17b21556d5ef
Igor Vlasov <vlasov.work@gmail.com>
2021-02-13 14:19:30 +0000

    решение задачи 02 и 04

---
+++ qna04.py
@@ -0,0 +1,18 @@
+x = int( input() )
+x1 = x // 1000
+x2 = x // 100
+x3 = x // 10 % 10
+x4 = x % 10
+
+x12 = x1 * x2
+x23 = x2 * x3
+x34 = x3 * x4
+
+if x12 % 3 == 0 and x23 % 5 == 0 and x34 % 10 == 0:
+  print(x12 + x23 + x34)
+elif x12 % 3 == 0:
+  print( 0 )
+elif x23 % 5 == 0:
+  print( 1 )
+elif x34 % 10 == 0:
+  print( 2 )
