e9298a897c685bce3569e296277c335b07dcd1cf
Tixdim <tixdim.on@gmail.com>
2021-02-13 13:55:56 +0000

    Я доделал 5 и тем самым сдал все задачи

--- 5.py
+++ 5.py
@@ -1,1 +1,12 @@
-a = input()
+q = input()
+a, b, c, d = q[0], q[1], q[2], q[3]
+w = []
+w.append(int(a))
+w.append(int(b))
+w.append(int(c))
+w.append(int(d))
+max1, min1, maxind, minind = max(w), min(w), w.index(max(w)), w.index(min(w))
+w[maxind], w[minind] = min1, max1
+for i in range(len(w) - 1):
+  print(w[i], end=" ")
+print(w[-1])
